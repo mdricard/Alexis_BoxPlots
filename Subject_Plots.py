aa559303b9781a0a0a05d984c63662d26b5721ce
Mark Ricard <ricard@uta.edu>
2025-06-22 19:26:20 -0500

    Initial commit

--- Subject_Plots.py
+++ Subject_Plots.py
@@ -7,4 +7,12 @@ csv = np.genfromtxt('D:/Alexis_Stats/Alexis_Final_Data_Juneteenth.csv', delimite
 
 # Convert NumPy array to Pandas DataFrame
 df = pd.DataFrame(csv, columns=['subject', 'shoe', 'speed', 'incline', 'step','comp_force','comp_impulse','shear_force', 'shear_impulse' ,'add_mom', 'add_impulse', 'trail_prop', 'lead_braking','hip_ron', 'knee_ron'])
-print(df.head())
+#print(df.head())
+# Select rows where col1 is greater than 2 AND col2 is 'C'
+#selected_rows = df[(df['col1'] > 2) & (df['col2'] == 'C')]
+#print(selected_rows)
+
+# Subset rows where shoe is Hoka, incline is level and speed is 0.5 m/s
+subset = df[(df['shoe'] == 0) & (df['incline'] == 0) & (df['speed'] == 0)]
+#subset = df[df['shoe'] == 0]
+print(subset.head())
